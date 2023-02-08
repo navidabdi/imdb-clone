cde1969f14428e716a9e4f721471160ecc22b10e
Nabi <artsanaat@gmail.com>
2023-02-08 23:14:04 +0300

    Fixed card ui component.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function Results({ results }) {
   return (
-    <div className="grid grid-cols-2 gap-2 max-w-[95%] lg:grid-cols-3 xl:grid-cols-4 md:max-w-[85%] mx-auto py-4">
+    <div className="grid grid-cols-2 gap-4 lg:gap-6 max-w-[95%] lg:grid-cols-3 xl:grid-cols-4 md:max-w-[85%] mx-auto py-4">
       {results.map((result) => (
         <Card key={result.id} result={result} />
       ))}
